94d49af0d2e0616bdef33d0f09ce1edf51a551d3
Raul José Chaves <rjchaves93@gmail.com>
2021-02-05 01:35:26 -0300

    incrementos na classe Rational

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 >
 src/Main.scala,b\b\bb4c4aa1aae1343bc1fb89a550895eebf1ed80f1
+B
+src/Rational.scala,4\c\4cbc42970fd094a174841ee9500c3bf00ba1586d
--- src/Rational.scala
+++ src/Rational.scala
@@ -1,27 +1,54 @@
+import scala.annotation.tailrec
+
+//usar implicit def intToRational(x: Int) = new Rational(x) para scala.language.implicitConversions
 class Rational (n: Int, d: Int){
   require(d != 0)
-  val numerator: Int = n
-  val denominator: Int = d
+  private val g = gcd(n.abs, d.abs)
+  val numerator: Int = n / g
+  val denominator: Int = d / g
 
 
   def this(n: Int)  = this(n, 1)
 
-  def add(that: Rational): Rational = {
+  def + (that: Rational): Rational =
     new Rational(
       numerator * that.denominator + that.numerator * denominator,
       denominator * that.denominator
     )
-  }
 
-  def +(that: Rational): Rational = add(that)
+  def + (that: Int) : Rational =
+    new Rational(numerator + that * denominator, denominator )
 
-  def lessThan(that:Rational): Boolean =
+  def - (that: Rational) : Rational =
+    new Rational(this.numerator*that.denominator - that.numerator * this.denominator,
+      this.denominator * that.denominator)
+
+  def - (that: Int) : Rational =
+    new Rational(this.numerator - that * denominator, denominator)
+
+  def * (that: Rational) : Rational =
+    new Rational(this.numerator * that.numerator, this.denominator * that.denominator)
+
+  def * (that: Int) : Rational =
+    new Rational(this.numerator * that, this.denominator)
+
+  def < (that:Rational): Boolean =
     this.numerator * that.denominator < that.numerator * this.denominator
 
-  def <(that:Rational): Boolean = lessThan(that)
+  def / (that: Rational) : Rational =
+    new Rational(this.numerator * that.denominator, that.denominator * this.denominator)
 
-  def max(that:Rational): Rational =
+  def / (that : Int) : Rational =
+    new Rational(this.numerator, this.denominator * that)
+
+  def max (that:Rational): Rational =
     if(this < that) that else this
 
   override def toString: String = s"$numerator/$denominator"
+
+  //algoritmo de euclides
+  @tailrec
+  private def gcd(i: Int, l: Int): Int = {
+    if(l == 0) i else gcd(l, i % l)
+  }
 }
