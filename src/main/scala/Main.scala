8bfdd2b853c2845223e005727c89180f7d8d41b8
Raul José Chaves <rjchaves93@gmail.com>
2021-03-03 02:20:40 -0300

    Migração para SBT e remoção do lixo

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,74 @@
+import dimensional.Spiral.spiral
+
+import java.io.{File, PrintWriter}
+import java.util.Date
+import scala.collection.mutable
+import scala.io.Source
+
+object Main extends App {
+//  val greetings = new Array[String](1)
+//  greetings(0) = "teste"
+//  greetings.update(0, "teste")
+//
+//  val twoThree = List( 2, 3)
+//  val oneTwoThree = 1 :: twoThree
+//  val oneTwo = 1 :: 2 :: 3 :: Nil
+//
+//  for (greet <- greetings)
+//    println(greet)
+//
+//  val l = (1, 2)
+//  l._1
+//  var s = Set(1, 2)
+//  s += 3
+//
+//  val s2 = mutable.Set(1, 2)
+//  s2 += 3
+//
+//  val name = "reader"
+//  println(s"Hello $name")
+//  println(s"Hello $name")
+//  println(
+//    s"""|Hello
+//       |$name""".stripMargin.trim)
+//
+//
+//
+//  def filelines(file: File) =
+//    Source.fromFile(file).getLines().toArray
+//
+//  val filesHere = new File("./src").listFiles()
+//
+//  def grep(pattern: String) = {
+//    for(
+//      file <- filesHere
+//      if file.getName.endsWith(".scala");
+//      line <- filelines(file);
+//      lineTrimmed = line.trim
+//      if lineTrimmed.matches(pattern)
+//    ) println(s"$file: ${lineTrimmed}")
+//  }
+//  grep(".*gcd.*")
+//
+//
+//  def echo(args : String*) = for(s <- args) println(s)
+//
+//  val seq = Seq("what's", "up")
+//  echo(seq: _*)
+//
+//  def withPrintWriter(file: File)(op: PrintWriter => Unit) = {
+//    val writer = new PrintWriter(file)
+//    try {
+//      op(writer)
+//    } finally {
+//      writer.close();
+//    }
+//  }
+//
+//  val file = new File("data.txt");
+//  withPrintWriter(file){ writer =>
+//    writer.println(new Date())
+//  }
+
+  println(spiral(200, 1))
+}
